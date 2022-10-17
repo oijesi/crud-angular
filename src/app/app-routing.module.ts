554827176094da92167d26e081a0e634699ccd90
Jesiel Azevedo <jesiellima@hotmail.com>
2022-10-17 01:15:28 -0300

    adicionando modulo e componente para itens e configurando rotas iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'items' },
+  {
+    path: 'items',
+    loadChildren: () => import('./items/items.module').then(m => m.ItemsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
