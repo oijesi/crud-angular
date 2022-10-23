ed9dd10ed90df1c2ebb92f2a32f8f25e148f188d
Jesiel Azevedo <jesiellima@hotmail.com>
2022-10-23 00:39:53 -0300

    adicionando service para items

--- src/app/items/items/items.component.ts
+++ src/app/items/items/items.component.ts
@@ -1,5 +1,7 @@
+import { ItemsService } from './../services/items.service';
 import { Item } from './../model/item';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-items',
@@ -8,14 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
-  items: Item[] = [
-    { _id: '1', name: 'Angular', category: 'Front-End' },
-    { _id: '2', name: 'Spring', category: 'Back-End' }
-  ];
+  items: Observable<Item[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  //itemsService: ItemsService;
+
+  constructor(private itemsService: ItemsService) {
     //this.items = [];
+    //this.itemsService = new ItemsService();
+    this.items = this.itemsService.list();
   }
 
   ngOnInit(): void {
---
+++ src/app/items/services/items.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs/operators';
+
+import { Item } from './../model/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+
+  private readonly API = '/assets/items.json'
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Item[]>(this.API)
+    .pipe(
+      first(),
+      tap(items => console.log(items))
+    );
+  }
+}
