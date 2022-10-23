b14314056503673c4a0c0c68a70861fc92c66a60
Jesiel Azevedo <jesiellima@hotmail.com>
2022-10-23 02:24:06 -0300

    criando pipe para exibição de ícone

---
+++ src/app/shared/pipes/category.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'category'
+})
+export class CategoryPipe implements PipeTransform {
+
+  transform(value: string): string {
+    switch(value.toLowerCase()) {
+        case 'front-end': return 'code';
+        case 'back-end': return 'computer';
+        case 'cloud': return 'cloud';
+    }
+    return 'code';
+  }
+
+}
